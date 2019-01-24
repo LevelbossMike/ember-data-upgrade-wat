5ac89fcf4d57b63b954b54d84e461d08428df871
Michael Klein <michael@firstiwaslike.com>
2019-01-24 16:04:25 +0100

    Load data via storefront

--- app/models/post.js
+++ app/models/post.js
@@ -2,13 +2,16 @@ import { readOnly } from '@ember/object/computed';
 import DS from 'ember-data';
 import { belongsTo } from 'ember-data/relationships';
 import attr from 'ember-data/attr';
+import LoadableModel from 'ember-data-storefront/mixins/loadable-model';
 
-export default DS.Model.extend({
+export default DS.Model.extend(LoadableModel, {
   author: belongsTo({ async: false }),
 
   title: attr('string'),
   text: attr('string'),
 
   zipCode: readOnly('author.address.zipCode'),
+  zipCodeId: readOnly('zipCode.id'),
+
   code: readOnly('zipCode.code')
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('post', { include: 'author.address.zip-code'});
+    return this.store.loadAll('post', { include: 'author.address.zip-code'});
   }
 });
